3ddf84e61ca41a97138c948a422c16d7112e7c85
Wanderlei Santos <wanderleisantosdias@gmail.com>
2017-05-06 22:53:41 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/wanderlei/blog/config/SpringDataConfig.java
@@ -0,0 +1,67 @@
+package br.com.wanderlei.blog.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * Created by wanderlei on 06/05/17.
+ */
+@Configuration
+@EnableJpaRepositories("br.com.wanderlei.blog.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+public class SpringDataConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager(EntityManagerFactory factory){
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(factory);
+        manager.setJpaDialect(new HibernateJpaDialect());
+        return manager;
+    }
+
+    @Bean
+    public HibernateJpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(environment.getProperty("hibernate.show.sql", Boolean.class));
+        adapter.setGenerateDdl(environment.getProperty("hibernate.ddl", Boolean.class));
+        return adapter;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        factoryBean.setPackagesToScan(environment.getProperty("hibernate.package.scan"));
+        factoryBean.setDataSource(dataSource());
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource(){
+        BasicDataSource dataSource =  new BasicDataSource();
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.pass"));
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        return dataSource;
+    }
+}
---
+++ src/main/java/br/com/wanderlei/blog/config/SpringMvcConfig.java
@@ -0,0 +1,10 @@
+package br.com.wanderlei.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wanderlei on 06/05/17.
+ */
+@Configuration
+public class SpringMvcConfig {
+}
